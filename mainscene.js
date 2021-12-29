1d7caeadd8885951bd6701920dadc89313b0ecbc
jocelynthiojaya <jocelynthiojaya@gmail.com>
2021-12-29 20:00:34 +0700

    fixed camera and plane, added assets

--- mainscene.js
+++ mainscene.js
@@ -8,6 +8,8 @@ var createDefaultEngine = function() { return new BABYLON.Engine(canvas, true, {
 const createScene =  () => {
     const scene = new BABYLON.Scene(engine);
 
+    // CAMERA ---------------------------
+
     // const camera = new BABYLON.ArcRotateCamera("Camera", - Math.PI/2, Math.PI/2, 0, new BABYLON.Vector3(0,2,0));
     // //2nd param is horizontal rotation
     // //3rd param is vertical rotation
@@ -15,7 +17,7 @@ const createScene =  () => {
     // camera.attachControl(canvas, true);
 
 
-    let camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(2, 2, 2), scene);
+    let camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(0, 2, 0), scene);
     camera.attachControl(canvas, true);
     camera.setTarget(BABYLON.Vector3.Zero());
 
@@ -41,47 +43,60 @@ const createScene =  () => {
     scene.collisionsEnabled = true;
     scene.workerCollisions = true;
 
-    camera.ellipsoid = new BABYLON.Vector3(1.5, 1, 1.5);
+    camera.ellipsoid = new BABYLON.Vector3(0.9, 1, 0.9);
     camera.checkCollisions = true;
     camera.applyGravity = true;
 
     
 
 
-
+    // SCENE ---------------------------
     const light1 = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(0, -1, 1));
     const light2 = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 1, 0));   
     light1.intensity =0.75;
     light2.intensity =0.5;
 
-    let plane = BABYLON.MeshBuilder.CreatePlane("ground", {
-        height: 30,
-        width: 50
-      }, scene)
-   
- 
-    plane.position.y = -0.2;
-    plane.rotate(BABYLON.Axis.X, Math.PI / 2, BABYLON.Space.WORLD);
+    // PLANE
+    var plane = BABYLON.MeshBuilder.CreatePlane("ground", {height: 30,width: 50}, scene)
+    //plane.position.y = 0;
+    //plane.rotate(BABYLON.Axis.X, Math.PI / 2, BABYLON.Space.WORLD);
+    plane.rotation.x = Math.PI / 2;
     plane.collisionsEnabled = true;
     plane.checkCollisions = true;
 
+    // ROOM
     var room = BABYLON.SceneLoader.Append("Assets/Room/", "scene.gltf", scene, function (a) {
-        // Create a default arc rotate camera and light.
-        //scene.createDefaultCameraOrLight(true, true, true);
-
-        // The default camera looks at the back of the asset.
-        // Rotate the camera by 180 degrees to the front of the asset.
-        //scene.activeCamera.alpha += Math.PI;
         console.log(a);
-        for (mesh in a.meshes){
-            // x
-            a.meshes[mesh].position.x = 1.5
-            a.meshes[mesh].position.y = 0
-        }
+        const roommesh = a.meshes[0];
+        roommesh.position.x = 0;
+        roommesh.position.y = -0.1;
+        roommesh.position.z = 0;
         
     });
 
+    // STANDS
+    // red tablecloth
+    // var redmat = new BABYLON.StandardMaterial("redmat", scene);
+    // var redtexture = new BABYLON.Texture("Assets/redtablecloth.jpg", scene);
+    // redmat.diffuseTexture = redtexture;
 
+    // var StandFaceUV = [];
+	// StandFaceUV[0] = new BABYLON.Vector4(1, 0, 0, 1);
+    // StandFaceUV[1] = new BABYLON.Vector4(1, 0, 0.32, 1);
+    // StandFaceUV[2] = new BABYLON.Vector4(0, 0, 0.25, 1);
+
+    // var tablecloth = BABYLON.SceneLoader.Append("Assets/tablecloth/", "scene.gltf", scene, function (b) {
+    //     //console.log(a);
+    //     const tableclothmesh = b.meshes[0];
+    //     tableclothmesh.position.x = 0;
+    //     tableclothmesh.position.y = 1;
+    //     tableclothmesh.position.z = 0;
+        
+    // });
+
+    var stand1 = BABYLON.MeshBuilder.CreateBox("stand1", {height: 1, width: 1, depth: 3});
+    stand1.checkCollisions = true;
+    stand1.position = new BABYLON.Vector3(-10, 1, 0.25);
     
 
     return scene;
