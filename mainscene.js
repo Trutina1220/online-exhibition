c89ec1abf5ded3ed1f7c2b80fd5331b655061810
jocelynthiojaya <jocelynthiojaya@gmail.com>
2022-01-03 16:32:50 +0700

    added boundaries to walls

--- mainscene.js
+++ mainscene.js
@@ -60,43 +60,89 @@ const createScene =  () => {
     var plane = BABYLON.MeshBuilder.CreatePlane("ground", {height: 30,width: 50}, scene)
     //plane.position.y = 0;
     //plane.rotate(BABYLON.Axis.X, Math.PI / 2, BABYLON.Space.WORLD);
+    plane.position = new BABYLON.Vector3(0, -0.1, 0);
     plane.rotation.x = Math.PI / 2;
     plane.collisionsEnabled = true;
     plane.checkCollisions = true;
 
     // ROOM
-    var room = BABYLON.SceneLoader.Append("Assets/Room/", "scene.gltf", scene, function (a) {
-        console.log(a);
-        const roommesh = a.meshes[0];
-        roommesh.position.x = 0;
-        roommesh.position.y = -0.1;
-        roommesh.position.z = 0;
-        
-    });
-
-    // STANDS
-    // red tablecloth
-    // var redmat = new BABYLON.StandardMaterial("redmat", scene);
-    // var redtexture = new BABYLON.Texture("Assets/redtablecloth.jpg", scene);
-    // redmat.diffuseTexture = redtexture;
-
-    // var StandFaceUV = [];
-	// StandFaceUV[0] = new BABYLON.Vector4(1, 0, 0, 1);
-    // StandFaceUV[1] = new BABYLON.Vector4(1, 0, 0.32, 1);
-    // StandFaceUV[2] = new BABYLON.Vector4(0, 0, 0.25, 1);
-
-    // var tablecloth = BABYLON.SceneLoader.Append("Assets/tablecloth/", "scene.gltf", scene, function (b) {
-    //     //console.log(a);
-    //     const tableclothmesh = b.meshes[0];
-    //     tableclothmesh.position.x = 0;
-    //     tableclothmesh.position.y = 1;
-    //     tableclothmesh.position.z = 0;
+    // var room = BABYLON.SceneLoader.Append("Assets/Room/", "scene.gltf", scene, function (a) {
+    //     console.log(a);
+    //     const roommesh = a.meshes[0];
+    //     roommesh.position.x = 0;
+    //     roommesh.position.y = -0.1;
+    //     roommesh.position.z = 0;
         
     // });
 
+    var room = BABYLON.SceneLoader.ImportMesh("", "Assets/Room/", "scene.gltf", scene, function (newMeshes) {
+
+        for (mesh in newMeshes) {
+            //newMeshes[mesh].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
+            newMeshes[mesh].position = new BABYLON.Vector3(0, 0, 0);
+            newMeshes[mesh].collisionsEnabled = true;
+            newMeshes[mesh].checkCollisions = true;
+            //newMeshes[mesh].rotation.y = Math.PI/2 + Math.PI/2 + Math.PI/2 ;
+            //newMeshes[mesh].actionManager = new BABYLON.ActionManager(scene);
+          
+        }
+        
+    });
+
+    // WALLS
+    var wall1 = BABYLON.MeshBuilder.CreateBox("wall1", {height: 13, width: 20.25, depth: 0.1}, scene)
+    //plane.rotate(BABYLON.Axis.X, Math.PI / 2, BABYLON.Space.WORLD);
+    wall1.position = new BABYLON.Vector3(-5.6, 0, 10.35);
+    //plane.rotation.x = Math.PI / 2;
+    wall1.collisionsEnabled = true;
+    wall1.checkCollisions = true;
+    wall1.isVisible = false;
+
+    var wall2 = BABYLON.MeshBuilder.CreateBox("wall2", {height: 10, width: 20.25, depth: 0.1}, scene)
+    wall2.position = new BABYLON.Vector3(-5.6, 0, -9.85);
+    wall2.collisionsEnabled = true;
+    wall2.checkCollisions = true;
+    wall2.isVisible = false;
+
+    var wall3 = BABYLON.MeshBuilder.CreateBox("wall3", {height: 13, width: 20.20, depth: 0.1}, scene)
+    wall3.position = new BABYLON.Vector3(4.5, 0, 0.2);
+    wall3.rotation.y = Math.PI / 2;
+    wall3.collisionsEnabled = true;
+    wall3.checkCollisions = true;
+    wall3.isVisible = false;
+
+    var wall4 = BABYLON.MeshBuilder.CreateBox("wall4", {height: 13, width: 20.20, depth: 0.1}, scene)
+    wall4.position = new BABYLON.Vector3(-15.8, 0, 0.2);
+    wall4.rotation.y = Math.PI / 2;
+    wall4.collisionsEnabled = true;
+    wall4.checkCollisions = true;
+    wall4.isVisible = false;
+
+    var ceiling = BABYLON.MeshBuilder.CreateBox("ceiling", {height: 20.5, width: 20.5, depth: 0.1}, scene)
+    ceiling.position = new BABYLON.Vector3(-5.7, 4.5, 0);
+    ceiling.rotation.x = Math.PI / 2;
+    ceiling.collisionsEnabled = true;
+    ceiling.checkCollisions = true;
+    ceiling.isVisible = false;
+
+
+    // STANDS
+
+    var tablecloth = BABYLON.SceneLoader.ImportMesh("", "Assets/tablecloth/", "scene.gltf", scene, function (newMeshes) {
+
+        for (mesh in newMeshes) {
+            newMeshes[mesh].scaling = new BABYLON.Vector3(2, 2, 2);
+            newMeshes[mesh].position = new BABYLON.Vector3(0, 3, 0);
+            newMeshes[mesh].rotation.y = Math.PI/2 + Math.PI/2 + Math.PI/2;
+            newMeshes[mesh].collisionsEnabled = true;
+            newMeshes[mesh].checkCollisions = true;
+        }
+        
+    });
+
     var stand1 = BABYLON.MeshBuilder.CreateBox("stand1", {height: 1, width: 1, depth: 3});
     stand1.checkCollisions = true;
-    stand1.position = new BABYLON.Vector3(-10, 1, 0.25);
+    stand1.position = new BABYLON.Vector3(-10, 0.5, 0.25);
     
 
     return scene;
